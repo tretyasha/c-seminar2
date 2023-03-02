342e18edbf2bc8062b68df145e363798e8a960ef
tretyasha <72045043+tretyasha@users.noreply.github.com>
2023-03-02 22:59:38 +0300

    задача2 решена

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+Console.WriteLine("Введите число X");
+int X = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число Y");
+int Y = int.Parse(Console.ReadLine());
+if (X > 0 && Y > 0)
+{
+    Console.WriteLine("Точка в первой четверти");
+}
+else if (X < 0 && Y > 0)
+{
+    Console.WriteLine("Точка во второй четверти");
+
+}
+else if (X < 0 && Y < 0)
+{
+    Console.WriteLine("Точка в третьей четверти");
+
+}
+else
+{
+    Console.WriteLine("Точка в четвертой четверти");
+}
---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Программа проверяет пятизначное число на палиндромом.
+
+Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(Console.Readline());
+int firstDigit = number / 10000;
+
+int secondDigit = (number / 1000) - 10 * firstDigit;
+
+int fifthDigit = number % 10;
+
+int fourthDigit = ((number % 100) - fifthDigit) / 10;
+
+if (firstDigit == fifthDigit && secondDigit == fourthDigit)
+    Console.WriteLine("Введенное число является палиндромом");
+else
+    Console.WriteLine("Введенное число не является палиндромом");
