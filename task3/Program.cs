355cd4ae89b49269ac4c39f4320d0597e41b351f
tretyasha <72045043+tretyasha@users.noreply.github.com>
2023-03-02 23:00:02 +0300

    задача3 решена

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,16 +1,14 @@
-﻿//Программа проверяет пятизначное число на палиндромом.
+﻿Console.WriteLine("Введите пятизначное число:");
 
-Console.WriteLine("Введите пятизначное число:");
-int number = int.Parse(Console.Readline());
+int number = int.Parse(Console.ReadLine());
+
 int firstDigit = number / 10000;
-
-int secondDigit = (number / 1000) - 10 * firstDigit;
-
-int fifthDigit = number % 10;
-
-int fourthDigit = ((number % 100) - fifthDigit) / 10;
+int secondDigit = (number - firstDigit * 10000) / 1000;
+int thirdDigit = (number - firstDigit * 10000 - secondDigit * 1000) / 100;
+int fourthDigit = (number - firstDigit * 10000 - secondDigit * 1000 - thirdDigit * 100) / 10;
+int fifthDigit = number - firstDigit * 10000 - secondDigit * 1000 - thirdDigit * 100 - fourthDigit * 10;
 
 if (firstDigit == fifthDigit && secondDigit == fourthDigit)
-    Console.WriteLine("Введенное число является палиндромом");
+    Console.WriteLine("Вы ввели палиндром!");
 else
-    Console.WriteLine("Введенное число не является палиндромом");
+    Console.WriteLine("Число не палиндром"!);
